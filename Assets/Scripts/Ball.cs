5cf318194ecccd1779e36cb41adfa74982fb269c
ereldebel <erel.debel@mail.huji.ac.il>
2022-04-27 13:13:02 +0300

    Fixed arena rotation and normalized ball position for the border movement to work. added rigidbody to ball.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,21 +2,21 @@
 
 public class Ball : MonoBehaviour
 {
-	public bool Grounded => Physics.CheckSphere(_transform.position, _checkSphereRadius);
+	public bool Grounded => Physics.CheckSphere(_rigidbody.position, _checkSphereRadius);
 	
-	public float XPosition => _transform.position.x;
+	public float XPosition => _rigidbody.position.x;
 
 	[Tooltip("The distance of the ball from the ground that the ball is already considered grounded.")] [SerializeField]
 	private float groundedDistance = 0.1f;
 
 	private SphereCollider _collider;
-	private Transform _transform;
+	private Rigidbody _rigidbody;
 	private float _checkSphereRadius;
 	
 	private void Awake()
 	{
 		_collider = GetComponent<SphereCollider>();
-		_transform = transform;
+		_rigidbody = GetComponent<Rigidbody>();
 		_checkSphereRadius = _collider.radius + groundedDistance;
 	}
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,26 +1,32 @@
-using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] private Ball ball;
-	[SerializeField] private Material arenaMaterial;
+	[SerializeField] private GameObject arena;
 	[SerializeField] private string arenaMaterialBorderXValueName = "BorderX";
-	[SerializeField] private float borderWidth;
+	[SerializeField] private float borderRelativeWidth;
 	[SerializeField] private float borderChangeSpeed = 0.01f;
 
 	private static GameManager _shared;
 
 	private float _borderX = 0.5f;
+	private Material _arenaMaterial;
+	private float _arenaWidth;
+	private const float PlaneWidth = 10;
+	private int _shaderBorderXVar;
 
 	private void Awake()
 	{
 		_shared = this;
+		_arenaMaterial = arena.GetComponent<Renderer>().material;
+		_arenaWidth = arena.transform.localScale.x * PlaneWidth;
+		_shaderBorderXVar = Shader.PropertyToID(arenaMaterialBorderXValueName);
 	}
 
 	private void OnValidate()
 	{
-		borderWidth /= 2;
+		borderRelativeWidth /= 2;
 	}
 
 	private void Update()
@@ -30,9 +36,11 @@ public class GameManager : MonoBehaviour
 
 	private void UpdateBorder()
 	{
-		if (!ball.Grounded || Mathf.Abs(ball.XPosition - _borderX) < borderWidth) return;
+		if (!ball.Grounded) return;
+		var normalizedXPosition = ball.XPosition / _arenaWidth + 0.5f;
+		if (Mathf.Abs(normalizedXPosition - _borderX) < borderRelativeWidth) return;
 		var change = Time.deltaTime * borderChangeSpeed;
-		_borderX += ball.XPosition < _borderX ? change : -change;
-		arenaMaterial.SetFloat(arenaMaterialBorderXValueName, _borderX);
+		_borderX += normalizedXPosition < _borderX ? -change : change;
+		_arenaMaterial.SetFloat(_shaderBorderXVar, _borderX);
 	}
 }
