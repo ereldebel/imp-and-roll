1fa34e0047cc9affc246b51fd49d9897f1335ce5
ereldebel <erel.debel@mail.huji.ac.il>
2022-04-27 09:19:11 +0300

    Added arena shader and implemented it in Erel scene

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,4 +2,25 @@
 
 public class Ball : MonoBehaviour
 {
+	public bool Grounded => Physics.CheckSphere(_transform.position, _checkSphereRadius);
+	
+	public float XPosition => _transform.position.x;
+
+	[Tooltip("The distance of the ball from the ground that the ball is already considered grounded.")] [SerializeField]
+	private float groundedDistance = 0.1f;
+
+	private SphereCollider _collider;
+	private Transform _transform;
+	private float _checkSphereRadius;
+	
+	private void Awake()
+	{
+		_collider = GetComponent<SphereCollider>();
+		_transform = transform;
+		_checkSphereRadius = _collider.radius + groundedDistance;
+	}
+
+	private void OnValidate()
+	{
+	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,38 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 	[SerializeField] private Ball ball;
+	[SerializeField] private Material arenaMaterial;
+	[SerializeField] private string arenaMaterialBorderXValueName = "BorderX";
+	[SerializeField] private float borderWidth;
+	[SerializeField] private float borderChangeSpeed = 0.01f;
+
 	private static GameManager _shared;
+
+	private float _borderX = 0.5f;
 
 	private void Awake()
 	{
 		_shared = this;
+	}
+
+	private void OnValidate()
+	{
+		borderWidth /= 2;
+	}
+
+	private void Update()
+	{
+		UpdateBorder();
+	}
+
+	private void UpdateBorder()
+	{
+		if (!ball.Grounded || Mathf.Abs(ball.XPosition - _borderX) < borderWidth) return;
+		var change = Time.deltaTime * borderChangeSpeed;
+		_borderX += ball.XPosition < _borderX ? change : -change;
+		arenaMaterial.SetFloat(arenaMaterialBorderXValueName, _borderX);
 	}
 }
