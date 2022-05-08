4ff380b2281d33664b5c94584bb98ce31bf87d11
ereldebel <erel.debel@mail.huji.ac.il>
2022-05-08 21:07:09 +0300

    Updated Spectral Chain.

--- Assets/Scripts/CameraDistance.cs
+++ Assets/Scripts/CameraDistance.cs
@@ -5,7 +5,7 @@ public class CameraDistance : MonoBehaviour
 {
 	[SerializeField] private float dist;
 
-	private void Update()
+	private void OnValidate()
 	{
 		if (dist == 0) return;
 		transform.position = transform.position.normalized * dist;
