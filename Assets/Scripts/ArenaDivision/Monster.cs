794d5a3c0c25c6365145c89a6fb160fe1750348b
erankramf <eran.kramf@mail.huji.ac.il>
2022-05-23 16:19:07 +0300

    Changed monster speed growth to Log

--- Assets/Scripts/ArenaDivision/Monster.cs
+++ Assets/Scripts/ArenaDivision/Monster.cs
@@ -81,8 +81,8 @@ namespace ArenaDivision
 				enabled = false;
 			if (constantSpeeds) return;
 			// ReSharper disable once PossibleLossOfFraction
-			var speedMultiplier =
-				(int) ((Time.time - _startTime) / timeToSpeedUp) * speedUpMultiplier + 1; // TODO keep looking at values
+			var speedMultiplier = Mathf.Log10(1+(int) ((Time.time - _startTime) / timeToSpeedUp)) 
+				* speedUpMultiplier + 1; // TODO keep looking at values
 			_speed = _fixedBaseSpeed * speedMultiplier;
 			_ySpeed = _fixedBaseYSpeed * speedMultiplier;
 		}
