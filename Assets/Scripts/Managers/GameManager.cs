bbb18fbf776685f843c488e7371239f92bda79ad
erankramf <eran.kramf@mail.huji.ac.il>
2022-06-11 19:07:06 +0300

    Reset after winning works (at least with 1 player)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -99,7 +99,6 @@ namespace Managers
 				{
 					huds[0].SetActive(true);
 					emptyHudAnimator.gameObject.SetActive(true);
-					// emptyHudAnimator.enabled = true;
 				}
 				else
 				{
@@ -108,6 +107,7 @@ namespace Managers
 						hud.SetActive(true);
 					}
 				}
+				powerUps.SetActive(true);
 			}
 
 
